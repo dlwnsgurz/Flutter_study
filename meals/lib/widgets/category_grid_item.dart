8ebfb13bed5ce7a19345667f2d345854dfcf08fe
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-03-02 14:00:12 +0900

    GestureDetector -> InkWell

--- meals/lib/widgets/category_grid_item.dart
+++ meals/lib/widgets/category_grid_item.dart
@@ -10,26 +10,31 @@ class CategoryGridItem extends StatelessWidget {
   final Category category;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.55),
-              category.color.withOpacity(0.9),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-        ),
-        child: Text(
-          category.title,
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w700,
-            color: Theme.of(context).colorScheme.onBackground,
-          ),
-        ));
+          child: Text(
+            category.title,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w700,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          )),
+    );
   }
 }
