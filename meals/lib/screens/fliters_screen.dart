9240935d8581195f7f597e37098f7bf4e53bace2
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-03-03 20:47:14 +0900

    FilterScreen 위젯 -> stateless로 수정

--- meals/lib/screens/fliters_screen.dart
+++ meals/lib/screens/fliters_screen.dart
@@ -2,145 +2,113 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/providers/filter_provider.dart';
 
-class FiltersScreen extends ConsumerStatefulWidget {
+class FiltersScreen extends ConsumerWidget {
   const FiltersScreen({
     super.key,
   });
 
   @override
-  ConsumerState<FiltersScreen> createState() => _FiltersScreenState();
-}
-
-class _FiltersScreenState extends ConsumerState<FiltersScreen> {
-  var _glutenFreeFilterSet = false;
-  var _lactoseFreeFilterSet = false;
-  var _vegetarianFreeFilterSet = false;
-  var _veganFreeFilterSet = false;
-
-  @override
-  void initState() {
-    super.initState();
-    final activeFilters = ref.read(filterProvider);
-    _glutenFreeFilterSet = activeFilters[Filter.gluetenFree]!;
-    _lactoseFreeFilterSet = activeFilters[Filter.lactoseFree]!;
-    _vegetarianFreeFilterSet = activeFilters[Filter.vegetarian]!;
-    _veganFreeFilterSet = activeFilters[Filter.vegan]!;
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final activeFilters = ref.watch(filterProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Filters"),
       ),
-      body: PopScope(
-        canPop: false,
-        onPopInvoked: (didPop) {
-          if (didPop) return;
-          ref.read(filterProvider.notifier).setFilters({
-            Filter.gluetenFree: _glutenFreeFilterSet,
-            Filter.lactoseFree: _lactoseFreeFilterSet,
-            Filter.vegan: _veganFreeFilterSet,
-            Filter.vegetarian: _vegetarianFreeFilterSet,
-          });
-          Navigator.of(context).pop();
-        },
-        child: Column(
-          children: [
-            SwitchListTile(
-              onChanged: (isChecked) {
-                setState(() {
-                  _glutenFreeFilterSet = isChecked;
-                });
-              },
-              value: _glutenFreeFilterSet,
-              title: Text(
-                "Gluten Free",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-              ),
-              subtitle: Text(
-                "Only Include Gluten-Free Meals",
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  fontSize: 14,
-                ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            onChanged: (isChecked) {
+              ref
+                  .read(filterProvider.notifier)
+                  .setFilter(Filter.gluetenFree, isChecked);
+            },
+            value: activeFilters[Filter.gluetenFree]!,
+            title: Text(
+              "Gluten Free",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
-            SwitchListTile(
-              onChanged: (isChecked) {
-                setState(() {
-                  _lactoseFreeFilterSet = isChecked;
-                });
-              },
-              value: _lactoseFreeFilterSet,
-              title: Text(
-                "Lactose Free",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-              ),
-              subtitle: Text(
-                "Only Include Lactose-Free Meals",
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  fontSize: 14,
-                ),
+            subtitle: Text(
+              "Only Include Gluten-Free Meals",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.tertiary,
+                fontSize: 14,
               ),
             ),
-            SwitchListTile(
-              onChanged: (isChecked) {
-                setState(() {
-                  _vegetarianFreeFilterSet = isChecked;
-                });
-              },
-              value: _vegetarianFreeFilterSet,
-              title: Text(
-                "Vegetarian",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-              ),
-              subtitle: Text(
-                "Only Include Vegetarian Meals",
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  fontSize: 14,
-                ),
+          ),
+          SwitchListTile(
+            onChanged: (isChecked) {
+              ref
+                  .read(filterProvider.notifier)
+                  .setFilter(Filter.lactoseFree, isChecked);
+            },
+            value: activeFilters[Filter.lactoseFree]!,
+            title: Text(
+              "Lactose Free",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
-            SwitchListTile(
-              onChanged: (isChecked) {
-                setState(() {
-                  _veganFreeFilterSet = isChecked;
-                });
-              },
-              value: _veganFreeFilterSet,
-              title: Text(
-                "Vegan",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-              ),
-              subtitle: Text(
-                "Only Include Vegen Meals",
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  fontSize: 14,
-                ),
+            subtitle: Text(
+              "Only Include Lactose-Free Meals",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.tertiary,
+                fontSize: 14,
               ),
             ),
-          ],
-        ),
+          ),
+          SwitchListTile(
+            onChanged: (isChecked) {
+              ref
+                  .read(filterProvider.notifier)
+                  .setFilter(Filter.vegetarian, isChecked);
+            },
+            value: activeFilters[Filter.vegetarian]!,
+            title: Text(
+              "Vegetarian",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+            subtitle: Text(
+              "Only Include Vegetarian Meals",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.tertiary,
+                fontSize: 14,
+              ),
+            ),
+          ),
+          SwitchListTile(
+            onChanged: (isChecked) {
+              ref
+                  .read(filterProvider.notifier)
+                  .setFilter(Filter.vegan, isChecked);
+            },
+            value: activeFilters[Filter.vegan]!,
+            title: Text(
+              "Vegan",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+            subtitle: Text(
+              "Only Include Vegen Meals",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.tertiary,
+                fontSize: 14,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
