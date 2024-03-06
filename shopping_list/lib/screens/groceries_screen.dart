0c4c00e08d1063e8618762ea3211376266e9e5ce
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-03-06 21:28:38 +0900

    http 패키지 설치

--- shopping_list/lib/screens/groceries_screen.dart
+++ shopping_list/lib/screens/groceries_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_item.dart';
 import 'package:shopping_list/screens/new_item.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/grocery_item.dart';
