1f7f5938a73565952148791594dcc122553a6da1
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-03-20 23:51:58 +0900

    Image Picker 추가

--- favorite_places/lib/widgets/image_input.dart
+++ favorite_places/lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,18 +11,43 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+        source: ImageSource.gallery, maxHeight: 600);
+
+    if (pickedImage == null) return;
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: const Text("Take Picture"),
+    );
+    if (_selectedImage != null) {
+      mainContent = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
     return Container(
       decoration: BoxDecoration(
         border: Border.all(color: Theme.of(context).colorScheme.onPrimary),
       ),
       alignment: Alignment.center,
       height: 250,
-      child: TextButton.icon(
-          onPressed: () {},
-          icon: const Icon(Icons.camera),
-          label: const Text("Take Picture")),
+      child: mainContent,
     );
   }
 }
