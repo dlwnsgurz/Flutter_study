fa85e3c0db95280026d743a87cd96590f1f3f59e
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-03-22 22:20:42 +0900

    현재 위치 위도&경도 print

--- favorite_places/lib/screens/places_list_screen.dart
+++ favorite_places/lib/screens/places_list_screen.dart
@@ -1,10 +1,8 @@
-import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/place_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod/riverpod.dart';
 
 class PlacesListScreen extends ConsumerWidget {
   const PlacesListScreen({super.key});
---
+++ favorite_places/lib/widgets/location_input.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+      _isGettingLocation = true;
+    });
+    locationData = await location.getLocation();
+    setState(() {
+      _isGettingLocation = false;
+    });
+
+    print(locationData.latitude);
+    print(locationData.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      "위치 정보가 없습니다",
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+    );
+
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(color: Theme.of(context).colorScheme.onPrimary),
+          ),
+          alignment: Alignment.center,
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentLocation,
+              icon: const Icon(Icons.location_on),
+              label: const Text("현재 위치 불러오기"),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.map),
+              label: const Text("지도에서 선택하세요."),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
